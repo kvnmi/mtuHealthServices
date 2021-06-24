e37d3bcd1a58cda52502831e2840a4b177738dfc
TOMIWA <olujulotomiwagmail.com>
2021-06-24 11:35:02 +0100

    add main files

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AuthNavigator from "./App/Navigation/AuthNavigator";
+import MainNavigator from "./App/Navigation/MainNavigator";
+import AuthContext from "./App/auth/context";
+import AppLoading from "expo-app-loading";
+import storage from "./App/auth/storage";
+import NavigationTheme from "./App/Navigation/NavigationTheme";
 
 export default function App() {
+  const [user, setUser] = useState(); // Current User
+  const [isReady, setIsready] = useState(false);
+
+  const readUser = async () => {
+    const result = await storage.getToken();
+    if (!result) return;
+    setUser(result);
+  }; // Check for existing user
+
+  if (!isReady) {
+    return (
+      <AppLoading
+        startAsync={readUser}
+        onFinish={() => setIsready(true)}
+        onError={console.warn}
+      />
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContext.Provider value={{ user, setUser }}>
+      <NavigationContainer theme={NavigationTheme}>
+        {user ? <MainNavigator /> : <AuthNavigator />}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
