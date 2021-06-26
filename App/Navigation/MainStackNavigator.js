2d93bef6960f6edeae29fd53ec4cb3174266170e
TOMIWA <olujulotomiwagmail.com>
2021-06-26 21:00:48 +0100

    Changes before creating different navigatioln routes

--- App/Navigation/MainStackNavigator.js
+++ App/Navigation/MainStackNavigator.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import MainNavigator from "./patientNavigator/MainNavigator";
 import MainNurseNavigator from "./nurseNavigator/MainNurseNavigator";
